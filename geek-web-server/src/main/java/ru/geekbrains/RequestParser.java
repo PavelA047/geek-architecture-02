65d73c23fec5dca62f6dfc053f81313a613479c3
pavel <zzzzfogzzzz@gmail.com>
2022-08-08 04:24:55 +0600

    hw_2

--- geek-web-server/src/main/java/ru/geekbrains/RequestParser.java
+++ geek-web-server/src/main/java/ru/geekbrains/RequestParser.java
@@ -3,11 +3,25 @@ package ru.geekbrains;
 import ru.geekbrains.domain.HttpRequest;
 
 import java.util.Deque;
+import java.util.HashMap;
+import java.util.Map;
 
 public class RequestParser {
 
     public HttpRequest parse(Deque<String> rawRequest) {
-        // TODO
-        return new HttpRequest();
+
+        HttpRequest httpRequest = new HttpRequest();
+
+        String[] firstRawParts = rawRequest.pollFirst().split(" ");
+        httpRequest.setMethod(firstRawParts[0]);
+        httpRequest.setPath(firstRawParts[1]);
+
+        Map<String, String> headers = new HashMap<>();
+        rawRequest.stream().filter(r -> !r.isEmpty()).forEach((raw) -> {
+            String[] parts = raw.split(" ", 2);
+            headers.put(parts[0], parts[1]);
+        });
+        httpRequest.setHeaders(headers);
+        return httpRequest;
     }
 }
